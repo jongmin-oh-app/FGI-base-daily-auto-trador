85ff6b1fb69c5d44a902c143a83541fcf5b561a9
오종민 <jmoh.development@gmail.com>
2026-05-05 05:50:32 +0000

    Update run.sh and template.yaml to remove AWS_PROFILE references and change SSM parameter to koreainvestment-test

--- run.sh
+++ run.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-AWS_PROFILE=acdong
 STACK_NAME=FGI-daily-trader
 
 # 빌드
-sam build --profile $AWS_PROFILE
+sam build
 
 # 로컬 실행
-# sam local invoke LambdaFunction --profile $AWS_PROFILE
+# sam local invoke LambdaFunction
 
 # 배포
 sam deploy
 
-sam logs -n LambdaFunction --stack-name $STACK_NAME --tail --profile $AWS_PROFILE
+sam logs -n LambdaFunction --stack-name $STACK_NAME --tail
